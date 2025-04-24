52b6a9acce7a2ed18b684e606e101dca258782fa
Adish1313 <adishsingla64@gmail.com>
2025-04-24 16:43:26 +0530

    env

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS Configuration
 app.use(cors({
-    origin: ['http://172.16.13.138:3000', 'http://localhost:8080','https://healthoasis-website.vercel.app'],
+    origin: ['http://172.16.13.138:3000','http://localhost:8081', 'http://localhost:8080','https://healthoasis-website.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
